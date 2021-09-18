e21f95f679db41d26569a865a658512f3a9b36ff
Malte Koch <malte-koch@gmx.net>
2021-09-18 22:01:54 +0200

    Change padding

--- lib/note_views/appbar.dart
+++ lib/note_views/appbar.dart
@@ -235,7 +235,7 @@ class SortingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.only(top: 70),
+        padding: EdgeInsets.only(top: 75),
         child: Container(
           color: Theme.of(context).appBarTheme.backgroundColor,
           child: Row(
